56f190251c04d404fe687042d076cd62535e42ee
zanyvium <vzn123@live.dk>
2026-04-01 16:51:51 +0200

    fix: misunderstood that we should return identity on primes. Fixed to 'Whiz'

--- src/kata/code.py
+++ src/kata/code.py
@@ -1,5 +1,19 @@
 NUMBERS_UP_TO = 100
 
 
-def fizz_buzz_whiz(number: int):
-    return str(number)
+def is_prime(n: int) -> bool:
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+
+def fizz_buzz_whiz(number: int) -> str:
+    if is_prime(number):
+        return "Whiz"
