c23c429ee053229bedcd2bcc0e134c681e1904e5
zanyvium <vzn123@live.dk>
2026-04-01 17:10:52 +0200

    test: 3 & 5: FizzBuzz

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,4 +1,4 @@
-from src.kata.code import fizz_buzz_whiz
+from src.kata.code import NUMBERS_UP_TO, fizz_buzz_whiz
 
 # Code will return "Whiz" for primes
 # Code will return "Fizz" for divisible with 3
@@ -15,7 +15,18 @@ primes_up_to_100 = [
 ] 
 # fmt: on
 
+numbers_divisible_by_three = [x for x in range(3, NUMBERS_UP_TO + 1, 3)]
+numbers_divisible_by_five = [x for x in range(5, NUMBERS_UP_TO + 1, 5)]
+numbers_divisible_by_three_and_five = [
+    x for x in numbers_divisible_by_three if x in numbers_divisible_by_five
+]
+
 
 def test_primes():
     for number in primes_up_to_100:
         assert fizz_buzz_whiz(number) == "Whiz"
+
+
+def test_three_and_five():
+    for number in numbers_divisible_by_three_and_five:
+        assert fizz_buzz_whiz(number) == "FizzBuzz"
