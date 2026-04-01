e365950f164f7f33aab7430ea50c6601bd9fbe61
zanyvium <vzn123@live.dk>
2026-04-01 17:12:53 +0200

    update: test list updated with status

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,9 +1,9 @@
 from src.kata.code import NUMBERS_UP_TO, fizz_buzz_whiz
 
-# Code will return "Whiz" for primes
+# Code will return "Whiz" for primes <- COMPLETE
 # Code will return "Fizz" for divisible with 3
-# Code will return "Buzz" for divisible with 5
-# Code will return "FizzBuzz" for divisible with 3 and 5
+# Code will return "Buzz" for divisible with 5 <- NEXT!
+# Code will return "FizzBuzz" for divisible with 3 and 5 <- COMPLETE
 # Code will return identity if there are no other rules
 # Code will apply rules in correct restrictive order: -> prime -> 3&5 -> 5 -> 3 -> string of identity
 
