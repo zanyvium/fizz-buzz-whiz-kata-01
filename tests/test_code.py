6de0a636d5705000a4ca2570b6afd165851ba213
zanyvium <vzn123@live.dk>
2026-04-01 16:45:07 +0200

    test: returns identity on primes

--- tests/test_code.py
+++ tests/test_code.py
@@ -1,8 +1,15 @@
-from src.kata.code import NUMBERS_UP_TO, fizz_buzz_whiz
+from src.kata.code import fizz_buzz_whiz
 
 # Test will return identity for any number
+# fmt: off
+primes_up_to_100 = [
+    2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 
+    31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 
+    73, 79, 83, 89, 97
+] 
+# fmt: on
 
 
 def test_tmp_identity():
-    for number in range(1, NUMBERS_UP_TO + 1):
+    for number in primes_up_to_100:
         assert fizz_buzz_whiz(number) == number
